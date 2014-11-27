cf9d673bf9c84475d8b3e9df3dd6b1d10bcbb579
osxi <osxi@users.noreply.github.com>
2014-11-26 21:27:12 -0600

    Move #netVotes computed property to model.

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -1,7 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.ObjectController.extend({
-  netVotes: function() {
-    return this.get('model.upVotes') - this.get('model.downVotes');
-  }.property('downVotes', 'upVotes')
-});
+export default Ember.ObjectController.extend();
--- app/models/post.js
+++ app/models/post.js
@@ -3,5 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   upVotes: DS.attr('number', {defaultValue: 0}),
-  downVotes: DS.attr('number', {defaultValue: 0})
+  downVotes: DS.attr('number', {defaultValue: 0}),
+  netVotes: function() {
+    return this.get('upVotes') - this.get('downVotes');
+  }.property('downVotes', 'upVotes')
 });
